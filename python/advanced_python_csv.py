bbdb3c2392b6d35bdf6e3cad1d2dd92b9e1b61f4
Saniya Jesupaul <tosaniya@gmail.com>
2016-07-02 00:24:28 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,25 @@
 PLACE YOUR CODE HERE
+
+import csv
+
+def read_data(data):
+    with open(data) as csvfile:
+
+        rows = []
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            rows.append(', '.join(row))
+
+        for r in rows:
+            r.split(',')
+
+        emails = []
+        for r in rows:
+            temp = r.split(",")
+            emails.append(temp[3].strip())
+
+    outfile = open('emails.csv', 'w')
+    wr = csv.writer(outfile)
+    wr.writerows(zip(emails[1:]))
+
+read_data('faculty.csv')

714291b8d9d56e8434f59498397d8d618ad3ea16
Saniya Jesupaul <tosaniya@gmail.com>
2016-07-02 00:17:07 -0700

    Update q6_strings.py
    Need to fix NotImplementedErrors

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -1,8 +1,14 @@
 # Based on materials copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 
+import math
 
 def donuts(count):
+    if count < 10:
+        print("Number of donuts:", count)
+    else:
+        print("Number of donuts: many")
+        
     """
     Given an int count of a number of donuts, return a string of the
     form 'Number of donuts: <count>', where <count> is the number
@@ -18,10 +24,17 @@ def donuts(count):
     >>> donuts(99)
     'Number of donuts: many'
     """
-    raise NotImplementedError
+    #raise NotImplementedError
 
 
 def both_ends(s):
+    if len(s) < 2:
+        print('')
+    else:
+        s = list(s)
+        new_s = "".join(s[0:2] + s[len(s)-2:len(s)])
+        print(new_s)
+        
     """
     Given a string s, return a string made of the first 2 and the last
     2 chars of the original string, so 'spring' yields 'spng'.
@@ -37,10 +50,20 @@ def both_ends(s):
     >>> both_ends('xyz')
     'xyyz'
     """
-    raise NotImplementedError
+    #raise NotImplementedError
 
 
 def fix_start(s):
+    s = list(s)
+    f = s[0]
+    for i, c in enumerate(s):
+        if i != 0:
+            if c == f:
+                s[i] = '*'
+
+    s = "".join(s)
+    print(s)
+    
     """
     Given a string s, return a string where all occurences of its
     first char have been changed to '*', except do not change the
@@ -56,10 +79,25 @@ def fix_start(s):
     >>> fix_start('donut')
     'donut'
     """
-    raise NotImplementedError
+    #raise NotImplementedError
 
 
 def mix_up(a, b):
+    a = list(a)
+    b = list(b)
+
+    a1 = a[0]
+    a2 = a[1]
+    a[0] = b[0]
+    a[1] = b[1]
+    b[0] = a1
+    b[1] = a2
+
+    a = "".join(a)
+    b = "".join(b)
+
+    print(a, b)
+    
     """
     Given strings a and b, return a single string with a and b
     separated by a space '<a> <b>', except swap the first 2 chars of
@@ -74,10 +112,23 @@ def mix_up(a, b):
     >>> mix_up('pezzy', 'firm')
     'fizzy perm'
     """
-    raise NotImplementedError
+    #raise NotImplementedError
 
 
 def verbing(s):
+    length = len(s)
+    if length < 3:
+        print (s)
+
+    else:
+        s = list(s)
+        if "".join(s[length-3: length]) == "ing":
+            s.append('ly')
+        else:
+            s.append("ing")
+        s = "".join(s)
+        print (s)
+        
     """
     Given a string, if its length is at least 3, add 'ing' to its end.
     Unless it already ends in 'ing', in which case add 'ly' instead.
@@ -91,10 +142,22 @@ def verbing(s):
     >>> verbing('do')
     'do'
     """
-    raise NotImplementedError
+    #raise NotImplementedError
 
 
 def not_bad(s):
+    if ('not' in s and 'bad' in s):
+        start = s.index('not')
+        end = s.index('bad')+3
+        if (start < end):
+            s = list(s)
+            del s[start:end]
+            s.insert(start, 'good')
+            s = "".join(s)
+        print (s)
+    else:
+        print(s)
+        
     """
     Given a string, find the first appearance of the substring 'not'
     and 'bad'. If the 'bad' follows the 'not', replace the whole
@@ -111,10 +174,20 @@ def not_bad(s):
     >>> not_bad("It's bad yet not")
     "It's bad yet not"
     """
-    raise NotImplementedError
+    #raise NotImplementedError
 
 
 def front_back(a, b):
+    mid = math.ceil((len(a)/2))
+    a_front = a[0:mid]
+    a_back = a[mid: len(a)]
+
+    mid = math.ceil(len(b)/2)
+    b_front = b[0:mid]
+    b_back = b[mid: len(b)]
+
+    print (a_front+b_front+a_back+b_back)
+    
     """
     Consider dividing a string into two halves. If the length is even,
     the front and back halves are the same length. If the length is
@@ -130,4 +203,4 @@ def front_back(a, b):
     >>> front_back('Kitten', 'Donut')
     'KitDontenut'
     """
-    raise NotImplementedError
+    #raise NotImplementedError
